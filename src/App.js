792e0f61dee211b789227cf5156d13f451ca9627
haydarcayir <haydarcayir@haydarcayir-MacBook-Air.local>
2021-07-03 16:24:35 +0300

    deleted  unnecessary code and added materil-ui and sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Box from "@material-ui/core/Box";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Box>haydar</Box>
+    </div>
+  );
 }
 
 export default App;
